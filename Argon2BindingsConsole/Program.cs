c4bc4fc1da84e558916e3984ed6aa705bbc1deaa
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2021-11-30 19:47:09 -0500

    Remove Argon2Context local var (method will initialize the same default instance)

--- Argon2BindingsConsole/Program.cs
+++ Argon2BindingsConsole/Program.cs
@@ -1,21 +1,15 @@
-﻿using System.Text;
-using Argon2Bindings;
+﻿using Argon2Bindings;
 
 const string salt = "testing123";
 const string pass = "test";
-
-Argon2Context context = new();
-
-/*byte[] outEncodedBytes = Argon2Core.Hash(pass, salt, context);
-Console.WriteLine($"Encoded (B64): {Encoding.UTF8.GetString(outEncodedBytes)}");*/
 
 for (var i = 0; i < 5; ++i)
 {
     Console.WriteLine($"\nTEST: {i + 1}");
 
-    byte[] rawHashBytes = Argon2Core.HashRaw(pass, salt, context);
+    byte[] rawHashBytes = Argon2Core.HashRaw(pass, salt);
     Console.WriteLine($"RAW (HEX): {rawHashBytes.ToHexString()}");
 
-    string encodedHash = Argon2Core.HashEncoded(pass, salt, context);
+    string encodedHash = Argon2Core.HashEncoded(pass, salt);
     Console.WriteLine($"Encoded (B64): {encodedHash}");
 }
