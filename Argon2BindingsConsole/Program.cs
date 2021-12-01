0ea7f0599e50556966420b95a97ab5b165663dba
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2021-11-30 21:15:26 -0500

    Pretty print the results of rounds of hashing + list number of hashing failures

--- Argon2BindingsConsole/Program.cs
+++ Argon2BindingsConsole/Program.cs
@@ -3,13 +3,29 @@
 const string salt = "testing123";
 const string pass = "test";
 
+int rawHashFailures = 0, 
+    encodedHashFailures = 0;
+
+const string format = "{0}\t\t{1}\t\t{2}\t\t{3}\n";
+
+Console.WriteLine("Legend:\nR = Raw Hash\nE = Encoded Hash\n");
+Console.WriteLine("Run #\t\tType\t\tResult\t\tOutput\n");
+
 for (var i = 0; i < 5; ++i)
 {
-    Console.WriteLine($"\nTEST: {i + 1}");
+    var rawHashResult = Argon2Core.HashRaw(pass, salt);
+    if (rawHashResult.Result is not Argon2Result.Ok)
+        ++rawHashFailures;
+    
+    Console.Write(format, $"{i + 1}", "R (HEX)", rawHashResult.Result,
+        rawHashResult.HashBytes.ToHexString());
 
-    byte[] rawHashBytes = Argon2Core.HashRaw(pass, salt);
-    Console.WriteLine($"RAW (HEX): {rawHashBytes.ToHexString()}");
+    var encodedHashResult = Argon2Core.HashEncoded(pass, salt);
+    if (encodedHashResult.Result is not Argon2Result.Ok)
+        ++encodedHashFailures;
 
-    string encodedHash = Argon2Core.HashEncoded(pass, salt);
-    Console.WriteLine($"Encoded (B64): {encodedHash}");
+    Console.WriteLine(format, $"{i + 1}", "E (B64)", encodedHashResult.Result,
+        encodedHashResult.EncodedHash);
 }
+
+Console.WriteLine($"Total Raw Hash Failures: {rawHashFailures}\nTotal Encoded Hash Failures: {encodedHashFailures}");
