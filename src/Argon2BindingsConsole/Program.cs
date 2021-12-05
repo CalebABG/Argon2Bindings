5bc9a3b5f75c41db9c4c0c59e56aa11564c9610b
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2021-12-04 23:32:46 -0500

    Only print results if no error

--- src/Argon2BindingsConsole/Program.cs
+++ src/Argon2BindingsConsole/Program.cs
@@ -13,19 +13,19 @@ Console.WriteLine("Run #\t\tType\t\tResult\t\tOutput\n");
 
 for (var i = 0; i < 5; ++i)
 {
+    var runNum = $"{i + 1}";
+
     var rawHashResult = Argon2Core.HashRaw(pass, salt);
     if (rawHashResult.Status is not Argon2Result.Ok)
         ++rawHashFailures;
-
-    Console.Write(format, $"{i + 1}", "R (HEX)", rawHashResult.Status,
-        rawHashResult.RawHash.ToHexString());
+    else
+        Console.Write(format, runNum, "R (HEX)", rawHashResult.Status, rawHashResult.RawHash.ToHexString());
 
     var encodedHashResult = Argon2Core.HashEncoded(pass, salt);
     if (encodedHashResult.Status is not Argon2Result.Ok)
         ++encodedHashFailures;
-
-    Console.WriteLine(format, $"{i + 1}", "E (B64)", encodedHashResult.Status,
-        encodedHashResult.EncodedHash);
+    else
+        Console.WriteLine(format, runNum, "E (B64)", encodedHashResult.Status, encodedHashResult.EncodedHash);
 }
 
 Console.WriteLine($"Total Raw Hash Failures: {rawHashFailures}\nTotal Encoded Hash Failures: {encodedHashFailures}");
