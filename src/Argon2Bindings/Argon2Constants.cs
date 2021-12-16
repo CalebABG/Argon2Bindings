c1d97df7bd52bc625e482e06f2eafd357ed29155
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2021-12-16 07:25:09 -0500

    Make constants static class public

--- src/Argon2Bindings/Argon2Constants.cs
+++ src/Argon2Bindings/Argon2Constants.cs
@@ -9,7 +9,7 @@ namespace Argon2Bindings;
 /// A collection of constants for defining argon2
 /// defaults.
 /// </summary>
-internal static class Argon2Constants
+public static class Argon2Constants
 {
     public const uint DefaultTimeCost = 3;
     public const uint DefaultMemoryCost = 1 << 12;
