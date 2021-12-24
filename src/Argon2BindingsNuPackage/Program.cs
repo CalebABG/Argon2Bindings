a844f0ac29df8963ea67ab90d3a0658290954b35
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2021-12-24 01:34:09 -0500

    Update step numbers

--- src/Argon2BindingsNuPackage/Program.cs
+++ src/Argon2BindingsNuPackage/Program.cs
@@ -3,13 +3,12 @@
 // Steps:
 // 1. Package the Argon2Bindings Library
 // 2. Validate that the nuget package is in the `nupackages` folder
-// 3. Do a `dotnet restore Argon2Bindings.sln` to pickup the local bindings package
-// 4. Install the `Argon2Bindings` nuget package
+// 3. Install the `Argon2Bindings` nuget package
 //    - Nuget: (check `Prerelease` box)
 //    - CLI:
 //       - From Solution folder:  dotnet add src/Argon2BindingsNuPackage package Argon2Bindings --prerelease
 //       - From Project folder:   dotnet add package Argon2Bindings --prerelease
-// 5. Uncomment the code below, build and run
+// 4. Uncomment the code below, build and run
 public static class Program
 {
     public static void Main(string[] args)
