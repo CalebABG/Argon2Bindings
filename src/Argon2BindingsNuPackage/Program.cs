4770dcc87ceaa78f3607d919b50e319d2236b316
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2021-12-24 01:16:43 -0500

    Add csproj nuget package snippet

--- src/Argon2BindingsNuPackage/Program.cs
+++ src/Argon2BindingsNuPackage/Program.cs
@@ -4,7 +4,8 @@
 // 1. Package the Argon2Bindings Library
 // 2. Validate that the nuget package is in the `nupackages` folder
 // 3. Do a `dotnet restore Argon2Bindings.sln` to pickup the local bindings package
-// 4. Install the `Argon2Bindings` nuget package (check Prerelease box)
+// 4. Install the `Argon2Bindings` nuget package (check Prerelease box, update version if needed)
+//    - <ItemGroup><PackageReference Include="Argon2Bindings" Version="0.0.1-preview" /></ItemGroup>
 // 5. Uncomment the code below, build and run
 public static class Program
 {
