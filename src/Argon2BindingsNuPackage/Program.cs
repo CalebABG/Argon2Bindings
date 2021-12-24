f94b3c5d9c14f7d993ce1e041011e8778c5551b4
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2021-12-24 00:52:15 -0500

    Test less painful nuget package test

--- src/Argon2BindingsNuPackage/Program.cs
+++ src/Argon2BindingsNuPackage/Program.cs
@@ -1,4 +1,16 @@
-﻿using Argon2Bindings;
+﻿namespace Argon2BindingsNuPackage;
 
-var result = Argon2Core.Hash("test");
-Console.WriteLine(result);
+// Steps:
+// 1. Package the Argon2Bindings Library
+// 2. Validate that the nuget package is in the `nupackages` folder
+// 3. Do a `dotnet restore Argon2Bindings.sln` to pickup the local bindings package
+// 4. Install the `Argon2Bindings` nuget package (check Prerelease box)
+// 5. Uncomment the code below, and import the needed namespaces
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        /*var result = Argon2Core.Hash("test");
+        Console.WriteLine(result);*/
+    }
+}
