9950747aa0e2fba33d97c65ea0acf0f114e80fe7
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2022-03-02 23:37:51 -0500

    Remove Console.WriteLine from benchmark methods

--- tests/Argon2BindingsBenchmarks/Program.cs
+++ tests/Argon2BindingsBenchmarks/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Argon2Bindings;
 using Argon2Bindings.Enums;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 #if WINDOWS
 using BenchmarkDotNet.Diagnostics.Windows.Configs;
@@ -21,6 +20,9 @@ public static class Program
         private const string Salt = "testing123";
         private const string Password = "test";
 
+        private const string VerifyPasswordHash = "$argon2i$v=19$m=2048,t=3,p=1$dGVzdGluZzQ1Ng" +
+                                                  "$VaMcEYLV/tlKirCtI1MOF5UfaD6BCQvbTNggdHDVLNo";
+
         private static readonly Argon2Context Context = new()
         {
             Type = Argon2Type.Argon2Id,
@@ -31,31 +33,25 @@ public static class Program
         [Benchmark]
         public void HashRaw()
         {
-            var hash = Argon2Core.Hash(Password, Salt, Context, false);
-            Console.WriteLine(hash.EncodedHash);
+            Argon2Core.Hash(Password, Salt, Context, false);
         }
 
         [Benchmark]
         public void HashEncoded()
         {
-            var hash = Argon2Core.Hash(Password, Salt, Context);
-            Console.WriteLine(hash.EncodedHash);
+            Argon2Core.Hash(Password, Salt, Context);
         }
 
         [Benchmark]
         public void ContextHash()
         {
-            var hash = Argon2Core.ContextHash(Password, Salt, Context);
-            Console.WriteLine(hash.EncodedHash);
+            Argon2Core.ContextHash(Password, Salt, Context);
         }
 
         [Benchmark]
         public void Verify()
         {
-            var password = "test";
-            var encodedHash = "$argon2i$v=19$m=2048,t=3,p=1$dGVzdGluZzQ1Ng$VaMcEYLV/tlKirCtI1MOF5UfaD6BCQvbTNggdHDVLNo";
-            var result = Argon2Core.Verify(password, encodedHash);
-            Console.WriteLine(result.Success);
+            Argon2Core.Verify(Password, VerifyPasswordHash);
         }
     }
 
